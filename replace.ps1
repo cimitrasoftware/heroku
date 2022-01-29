91488781374d920e28a21e779cc3ed1d71f35787
Tay Kratzer <tay.kratzer@gmail.com>
2022-01-29 16:06:14 -0700

    update

---
+++ replace.ps1
@@ -0,0 +1,25 @@
+Param(
+[string] $ReplaceText
+)
+
+# TEMPLATE FILE
+$TemplateFile = "C:\Users\tay\heroku\Node-Heroku-Deployment\index_template.html"
+
+# DESTINATION INDEX FILE
+$IndexFile = "C:\Users\tay\heroku\Node-Heroku-Deployment\index.html"
+
+# CREATE A TEMPORARY FILE
+$TempFile = New-TemporaryFile
+
+# Copy the Template File to the Temporary File
+Copy-Item $TemplateFile $TempFile
+
+# Get the content of the Temp search and replace the REPLACE_TEXT string
+(Get-Content -path $TempFile  -Raw) -replace 'REPLACE_TEXT', $ReplaceText
+
+# Move the Temporary file to the index.html file
+Move-Item $TempFile $IndexFile -Force
+
+# Update via Git
+& "C:\Users\Tay\heroku\Node-Heroku-Deployment\update.ps1"
+
