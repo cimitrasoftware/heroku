7b37ef37b829d0d91f75f6761dea3cc12c7e683b
Tay Kratzer <tay.kratzer@gmail.com>
2022-01-29 18:11:13 -0700

    update

--- update.ps1
+++ update.ps1
@@ -1,7 +1,7 @@
 # git config --global credential.helper 'cache --timeout 108000000'
 # git config --global credential.helper store
 Set-Location C:\Users\tay\heroku\Node-Heroku-Deployment
-git pull
-git add .
-git commit -am "update" -q | Out-Null
-git push -q | Out-Null
+git pull > $null
+git add . > $null
+git commit -am "update" -q | Out-Null > $null
+git push -q | Out-Null > $null 
