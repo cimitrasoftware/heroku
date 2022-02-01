df8fe74dac1389f780211cf1ea461d19786dc148
unknown <tay@cimitra.com>
2022-02-01 11:28:33 -0700

    update

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const app = express();
 
-// app.get("/", function (req, res) {
-//   res.send("<html><body><h1> Tay Kratzer | Salesforce//Heroku Interview//February 1st, 2022</p></h1></body></html>");
-// });
-
 app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
